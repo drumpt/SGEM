6cdb30e5a55850bba3acc2c4ef6ec5cb014c79d0
server27 <gd7359@gmail.com>
2022-10-18 18:42:05 +0900

    Implement libri+noie, valentini dataset, before pull

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import random
 import gc
 import logging
 import pickle
+import shelve
 from datetime import datetime
 from copy import deepcopy
 import time
 from collections import deque
+import collections, functools, operator
 
 import hydra
 from omegaconf import OmegaConf, open_dict
@@ -361,61 +363,43 @@ def generate_adversarial_example(wavs, model, target_snr=15, lr=1e-4, n_steps=5)
 
 
 def get_instance_from_queue(args, method, wavs, probs):
-    out_wavs = [wav for wav in wavs]
+    out_wavs, out_hash_values = [], []
     if args.n_neighbors <= 0:
-        pass
+        selected_instances = []
     elif len(memory_queue) <= args.n_neighbors:
-        for wav, _ in list(memory_queue):
-            out_wavs.append(wav.to(wavs.device))
+        selected_instances = list(memory_queue)
     elif method == "random":
         selected_instances = random.sample(list(memory_queue), args.n_neighbors)
-        for wav, _ in selected_instances:
-            out_wavs.append(wav.to(wavs.device))
     elif method == "latest":
         selected_instances = list(memory_queue)[-args.n_neighbors:]
-        for wav, _ in selected_instances:
-            out_wavs.append(wav.to(wavs.device))
     elif method == "informative":
         new_mean_probs = torch.mean(probs.view(-1, probs.shape[-1]), dim=0)
         previous_instances = list(memory_queue)
         selected_instances = []
-        for wav, prob in previous_instances:
+        for wav, prob, hash_value in previous_instances:
             entropy = torch.mean(- torch.sum(prob * torch.log(prob), dim=-1)) # mean over tokens
             mean_probs = torch.mean(prob, dim=0).to(wavs.device)
             js_div = js_divergence(new_mean_probs, mean_probs)
-            selected_instances.append((wav, entropy / js_div))
-
-        print(f"args.n_neighbors : {args.n_neighbors}")
-
-        for wav, _ in sorted(selected_instances, key=lambda x: x[1], reverse=True)[:args.n_neighbors]:
-            out_wavs.append(wav.to(wavs.device))
+            selected_instances.append((wav, entropy / js_div, hash_value))
+        selected_instances = sorted(selected_instances, key=lambda x: x[1], reverse=True)[:args.n_neighbors]
     elif method == "similar":
         new_mean_probs = torch.mean(probs.view(-1, probs.shape[-1]), dim=0)
         previous_instances = list(memory_queue)
         selected_instances = []
-
-        from matplotlib import pyplot
-
-        entropy_list = []
-        # TODO: remove (just for checking)
-        for wav, prob in previous_instances:
-            print(f"prob.shape : {prob.shape}")
-            per_token_entropy = -torch.sum(prob * torch.log(prob), dim=-1)
-            for entropy in per_token_entropy:
-                entropy_list.append(entropy.detach().item())
-        pyplot.plot(entropy_list)
-        pyplot.savefig("entropy_distribution.png")
-
-        for wav, prob in previous_instances:
+        for wav, prob, hash_value in previous_instances:
             mean_probs = torch.mean(prob, dim=0).to(wavs.device)
             js_div = js_divergence(new_mean_probs, mean_probs)
-            selected_instances.append((wav, 1 / js_div))
+            selected_instances.append((wav, 1 / js_div, hash_value))
+        selected_instances = sorted(selected_instances, key=lambda x: x[1], reverse=True)[:args.n_neighbors]
 
-        for wav, _ in sorted(selected_instances, key=lambda x: x[1], reverse=True)[:args.n_neighbors]:
-            out_wavs.append(wav.to(wavs.device))
+    for wav, _, hash_value in selected_instances:
+        out_wavs.append(wav.to(wavs.device))
+        out_hash_values.append(hash_value)
 
-    out_wavs = pad_sequence(out_wavs, batch_first=True)
-    return out_wavs
+    if len(out_wavs) > 0:
+        out_wavs = pad_sequence(out_wavs, batch_first=True)
+    return out_wavs, out_hash_values
+
 
 def forward_and_adapt_ctc(args, model, teacher_model, processor, optimizer, scheduler, wavs, lens):
     outputs = model(wavs).logits
@@ -516,13 +500,6 @@ def forward_and_adapt_attn(args, model, teacher_model, processor, optimizer, sch
                     selected_frame = torch.where(softmax_entropy(log_prob_tensor, dim=-1) < args.entropy_threshold, 1, 0).bool()
                     e_loss = softmax_entropy(log_prob_tensor / args.temp)[selected_frame].mean(0).mean()
                 (args.em_coef / len(wavs) * e_loss).backward()
-
-                # print(f"(args.em_coef / len(wavs) * e_loss) : {(args.em_coef / len(wavs) * e_loss)}")
-                # print(f"model.require_grad : {model.require_grad}")
-                # grad_dic = {x[0]:x[1].data.grad for x in model.named_parameters()}
-                # for k, v in grad_dic.items():
-                #     print(f"{k} : {v}")
-                # for x in mode.named_parameters():
 
             if 1 - args.em_coef > 0:
                 c_loss = mcc_loss(log_prob_tensor / args.temp, reweight=args.reweight, class_num=1000)
@@ -635,60 +612,6 @@ def forward_and_adapt_attn(args, model, teacher_model, processor, optimizer, sch
             sum_log_probs = torch.sum(max_log_probs, dim=-1)
             nll_loss = - sum_log_probs.mean()
             (nll_loss / len(wavs)).backward()
-
-            # import pickle, shelve, json
-
-            # current = time.time()
-            # grad_dict = dict()
-            # for np, p in model.named_parameters():
-            #     grad_dict[np] = p.grad
-
-            # print(f"get_gradient 1 : {time.time() - current}")
-            # current = time.time()
-
-            # # with open("grad_example.pkl", "wb") as f:
-            # #     pickle.dump(grad_dict, f, pickle.HIGHEST_PROTOCOL)
-
-            # db = shelve.open("grad_example.pkl", writeback=True)
-            # db['grad'] = grad_dict
-
-            # # db = tshelve.open("grad_example.pkl", writeback=False)
-
-            # # with open("grad_example.json", "wt") as f:
-            # #     json.dump(grad_dict, f)
-
-            # # torch.save(grad_dict, "grad_example.pkl")
-
-            # # open("grad_example.pkl", "w").write(grad_dict)
-
-            # print(f"save_gradient : {time.time() - current}")
-            # current = time.time()
-
-            # # with open("grad_example.pkl", "rb") as f:
-            # #     new_grad_dict = pickle.load(f)
-
-            # new_grad_dict = db['grad']
-
-            # # with open("grad_example.pkl", "r") as f:
-            # #     new_grad_dict = f.read()
-
-            # # with open("grad_example.json", "rt") as f:
-            # #     new_grad_dict = json.load(grad_dict, f)
-
-            # # new_grad_dict = torch.load(grad_dict, "grad_example.pkl")
-
-            # print(f"load_gradient : {time.time() - current}")
-            # current = time.time()
-
-            # for np, p in model.named_parameters():
-            #     if new_grad_dict[np] != None:
-            #         p.grad = new_grad_dict[np].to(args.device)
-            #     else:
-            #         p.grad = None
-
-            # print(f"apply_gradient : {time.time() - current}")
-            # current = time.time()
-
     if "p_logp" in args.method:
         for wav in wavs:
             wav = wav.unsqueeze(0)
@@ -748,13 +671,73 @@ def forward_and_adapt_attn(args, model, teacher_model, processor, optimizer, sch
 
                 e_loss = - torch.sum(mean_prob * mean_log_prob, dim=-1).mean()
                 (e_loss / (len(wavs) * num_augs)).backward()
+    
 
     print(f"7-3 : {time.time() - current}")
     current = time.time()
 
+    # print(f"hash(wavs) : {hash(wavs)}")
+    # print(f"wavs.squeeze(0).shape : {wavs.squeeze(0).shape}")
+    # print(f"str(hash(wavs.squeeze(0))) : {str(hash(wavs.squeeze(0)))}")
+
+    if args.use_memory_queue:
+        global memory_queue, HASH_COUNTER
+
+        # get current gradient
+        current_grad_dict = dict()
+        for np, p in model.named_parameters():
+            current_grad_dict[np] = p.grad if p.grad == None else p.grad.cpu().clone()
+
+        # search wavs to adapt
+        probs = torch.softmax(log_prob_tensor, dim=-1)
+        wavs_to_adapt, hash_values_to_adapt = get_instance_from_queue(args, args.queue_method, wavs, probs)
+
+        # # for debugging
+        # for wav_to_adapt, hash_value_to_adapt in zip(wavs_to_adapt, hash_values_to_adapt):
+        #     print(f"wav_to_adapt.shape, hash_value_to_adapt : {wav_to_adapt.shape}, {hash_value_to_adapt}")
+
+        # # for debugging
+        # for wav_in_queue, _, hash_value_in_queue in list(memory_queue):
+        #     print(f"wav_in_queue.shape, hash_value_in_queue : {wav_in_queue.shape}, {hash_value_in_queue}")
+
+        print(f"7-5 : {time.time() - current}")
+        current = time.time()
+
+        if len(hash_values_to_adapt) > 0:
+            grad_dict_list = [db[hash_value_to_adapt] for hash_value_to_adapt in hash_values_to_adapt]
+            cumulated_grad = dict()
+            for np, _ in model.named_parameters():
+                grads_np = [grad_dict[np] for grad_dict in grad_dict_list]
+                cumulated_grad[np] = sum(grads_np) if not None in grads_np else None
+
+            denominator = len(wavs) + len(hash_values_to_adapt)
+            for np, p in model.named_parameters():  
+                if p.grad == None:
+                    continue
+                p.grad = p.grad * len(wavs) / denominator + cumulated_grad[np].to(args.device) / denominator
+
+        print(f"7-6 : {time.time() - current}")
+        current = time.time()
+
+        for wav, prob in zip(wavs, probs):
+            # dequeue
+            while len(memory_queue) >= args.queue_size:
+                wav_to_remove, _, hash_value_to_remove = memory_queue.popleft()
+                del db[hash_value_to_remove]
+
+            # enqueue
+            # hash_value = str(hash(wav.detach().cpu()))
+            hash_value = str(HASH_COUNTER)
+            memory_queue.append((wav.cpu().detach(), prob.cpu().detach(), hash_value))
+            db[hash_value] = current_grad_dict
+            HASH_COUNTER += 1
+
+        print(f"7-7 : {time.time() - current}")
+        current = time.time()
+
     optimizer.step()
 
-    print(f"7-4 : {time.time() - current}")
+    print(f"7-8 : {time.time() - current}")
     current = time.time()
 
     if scheduler is not None: 
@@ -834,6 +817,7 @@ def main(args):
     split = args.split
     batch_size = args.batch_size
     extra_noise = args.extra_noise
+    noise_type = args.noise_type
     sample_rate = args.sample_rate
 
     optimizer = args.optimizer
@@ -859,10 +843,17 @@ def main(args):
     queue_size = args.queue_size
     n_neighbors = args.n_neighbors
     if use_memory_queue:
-        global memory_queue
-        memory_queue = deque([], maxlen=queue_size)
+        assert steps == 1 and batch_size == 1
 
-    dataset = load_dataset(split, dataset_name, dataset_dir, batch_size, extra_noise)
+        global memory_queue, db, HASH_COUNTER
+        memory_queue = deque([], maxlen=queue_size)
+        time_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        if not os.path.exists("grad_dict"):
+            os.makedirs("grad_dict")
+        db = shelve.open(f"grad_dict/grads_{time_string}.pkl", writeback=True)
+        HASH_COUNTER = 0
+
+    dataset = load_dataset(split, dataset_name, dataset_dir, batch_size, extra_noise, noise_type=noise_type)
     gt_texts, ori_transcriptions, transcriptions_1, transcriptions_3, transcriptions_5, transcriptions_10, transcriptions_20, transcriptions_40 = [], [], [], [], [], [], [], []
 
     original_model = get_model(args, original=True)
@@ -890,8 +881,8 @@ def main(args):
     print(f"2 : {time.time() - current}")
     current = time.time()
 
-    # TODO: casuality
-    ori_wer_list, ada_wer_list, diff_wer_list, max_ent_list, avg_ent_list, max_ood_list, avg_ood_list = [], [], [], [], [], [], []
+    # # TODO: casuality
+    # ori_wer_list, ada_wer_list, diff_wer_list, max_ent_list, avg_ent_list, max_ood_list, avg_ood_list = [], [], [], [], [], [], []
 
     for batch_idx, batch in enumerate(dataset):
         lens, wavs, texts, _ = batch
@@ -909,45 +900,78 @@ def main(args):
         current = time.time()
 
         adapt_or_not = True
-        if args.use_memory_queue or args.selective_adaptation:
-            probs = []
-            for i, wav in enumerate(wavs):
-                wav = wav.unsqueeze(0)
-                if isinstance(model, Wav2Vec2ForCTC):
-                    logit = model(wav).logits
-                elif isinstance(model, EncoderDecoderASR):
-                    greedy_searcher = S2SRNNGreedySearcher(model.mods.decoder.emb, model.mods.decoder.dec, model.mods.decoder.fc, **{"bos_index": model.mods.decoder.bos_index, "eos_index": model.mods.decoder.eos_index, "min_decode_ratio": model.mods.decoder.min_decode_ratio, "max_decode_ratio": model.mods.decoder.max_decode_ratio,},).to(args.device)
-                    logit = forward_attn(args, model, greedy_searcher, wav, gt_wavs=None)
-                    logit = torch.stack(logit, dim=1)
-                elif isinstance(model, nemo_asr.models.EncDecRNNTBPEModel):
-                    logit = forward_trans(args, model, wav, torch.tensor([lens[i]]).to(wav.device), gt_wavs=None)
-                    logit = torch.stack(logit, dim=1)
-            probs.append(torch.softmax(logit.squeeze(0), dim=-1))
-            probs = pad_sequence(probs, batch_first=True).to(args.device)
-
-            if args.selective_adaptation:
+        if args.selective_adaptation:
+            with torch.no_grad():
+                probs = []
+                for i, wav in enumerate(wavs):
+                    wav = wav.unsqueeze(0)
+                    if isinstance(model, Wav2Vec2ForCTC):
+                        logit = model(wav).logits
+                    elif isinstance(model, EncoderDecoderASR):
+                        greedy_searcher = S2SRNNGreedySearcher(model.mods.decoder.emb, model.mods.decoder.dec, model.mods.decoder.fc, **{"bos_index": model.mods.decoder.bos_index, "eos_index": model.mods.decoder.eos_index, "min_decode_ratio": model.mods.decoder.min_decode_ratio, "max_decode_ratio": model.mods.decoder.max_decode_ratio,},).to(args.device)
+                        logit = forward_attn(args, model, greedy_searcher, wav, gt_wavs=None)
+                        logit = torch.stack(logit, dim=1)
+                    elif isinstance(model, nemo_asr.models.EncDecRNNTBPEModel):
+                        logit = forward_trans(args, model, wav, torch.tensor([lens[i]]).to(wav.device), gt_wavs=None)
+                        logit = torch.stack(logit, dim=1)
+                probs.append(torch.softmax(logit.squeeze(0), dim=-1))
+                probs = pad_sequence(probs, batch_first=True).to(args.device)
                 per_token_ood, _ = torch.max(probs, dim=-1)
                 per_token_odd = - per_token_ood
                 max_ood, _ = torch.max(per_token_odd, dim=-1) # max_ood per each instance
                 avg_max_ood = torch.mean(max_ood, dim=-1) # average max_ood per batch
-                if avg_max_ood < args.ood_threshold:
-                    adapt_or_not = False
+            if avg_max_ood < args.ood_threshold:
+                adapt_or_not = False
 
-            if args.use_memory_queue:
-                wavs_to_adapt = get_instance_from_queue(args, args.queue_method, wavs, probs)
-                lens_to_adapt = torch.tensor([len(wav) for wav in wavs_to_adapt]).to(args.device)
-            else:
-                wavs_to_adapt = wavs
-                lens_to_adapt = lens
-        else:
-            wavs_to_adapt = wavs
-            lens_to_adapt = lens
+            gc.collect()
+            torch.cuda.empty_cache()
+            del probs, per_token_ood, max_ood, avg_max_ood
+
+        # if args.use_memory_queue or args.selective_adaptation:
+        #     probs = []
+        #     for i, wav in enumerate(wavs):
+        #         wav = wav.unsqueeze(0)
+        #         if isinstance(model, Wav2Vec2ForCTC):
+        #             logit = model(wav).logits
+        #         elif isinstance(model, EncoderDecoderASR):
+        #             greedy_searcher = S2SRNNGreedySearcher(model.mods.decoder.emb, model.mods.decoder.dec, model.mods.decoder.fc, **{"bos_index": model.mods.decoder.bos_index, "eos_index": model.mods.decoder.eos_index, "min_decode_ratio": model.mods.decoder.min_decode_ratio, "max_decode_ratio": model.mods.decoder.max_decode_ratio,},).to(args.device)
+        #             logit = forward_attn(args, model, greedy_searcher, wav, gt_wavs=None)
+        #             logit = torch.stack(logit, dim=1)
+        #         elif isinstance(model, nemo_asr.models.EncDecRNNTBPEModel):
+        #             logit = forward_trans(args, model, wav, torch.tensor([lens[i]]).to(wav.device), gt_wavs=None)
+        #             logit = torch.stack(logit, dim=1)
+        #     probs.append(torch.softmax(logit.squeeze(0), dim=-1))
+        #     probs = pad_sequence(probs, batch_first=True).to(args.device)
+
+        #     if args.selective_adaptation:
+        #         per_token_ood, _ = torch.max(probs, dim=-1)
+        #         per_token_odd = - per_token_ood
+        #         max_ood, _ = torch.max(per_token_odd, dim=-1) # max_ood per each instance
+        #         avg_max_ood = torch.mean(max_ood, dim=-1) # average max_ood per batch
+        #         if avg_max_ood < args.ood_threshold:
+        #             adapt_or_not = False
+
+        #     if args.use_memory_queue:
+        #         wavs_to_adapt = get_instance_from_queue(args, args.queue_method, wavs, probs)
+        #         lens_to_adapt = torch.tensor([len(wav) for wav in wavs_to_adapt]).to(args.device)
+        #     else:
+        #         wavs_to_adapt = wavs
+        #         lens_to_adapt = lens
+        #     wavs_to_adapt = wavs
+        #     lens_to_adapt = lens
+        # else:
+        #     wavs_to_adapt = wavs
+        #     lens_to_adapt = lens
+
+        wavs_to_adapt = wavs
+        lens_to_adapt = lens
 
         print(f"5 : {time.time() - current}")
         current = time.time()
 
         gt_texts += texts
-        ori_transcription = transcribe_batch(args, original_model, processor, wavs, lens)
+        with torch.no_grad():
+            ori_transcription = transcribe_batch(args, original_model, processor, wavs, lens)
         ori_transcriptions += ori_transcription
         ori_wer = wer(list(texts), list(ori_transcription))
 
@@ -992,7 +1016,8 @@ def main(args):
             current = time.time()
 
             if step_idx in [1, 3, 5, 10, 20, 40]:
-                transcription = transcribe_batch(args, model, processor, wavs, lens)
+                with torch.no_grad():
+                    transcription = transcribe_batch(args, model, processor, wavs, lens)
                 transcription_list = eval(f"transcriptions_{step_idx}")
                 transcription_list += transcription
 
@@ -1040,11 +1065,12 @@ def main(args):
         #     df = pd.DataFrame(write_dict)
         #     df.to_csv(f"casuality_{args.dataset_name}_{list(args.method)}.csv", index=False)
 
-        if args.use_memory_queue and adapt_or_not:
-            for wav, prob in zip(wavs, probs):
-                while len(memory_queue) >= args.queue_size:
-                    memory_queue.popleft()
-                memory_queue.append((wav.cpu().detach(), prob.cpu().detach()))
+        # if args.use_memory_queue and adapt_or_not:
+        #     for wav, prob in zip(wavs, probs):
+        #         while len(memory_queue) >= args.queue_size:
+        #             wav_to_remove = memory_queue.popleft()[0]
+        #             db.pop(str(hash(wav_to_remove.detach().cpu())))
+        #         memory_queue.append((wav.cpu().detach(), prob.cpu().detach(), str(hash(wav.cpu().detach()))))
 
         if stochastic_restoration:
             for model_param, original_param in zip(model.parameters(), original_model.parameters()):
