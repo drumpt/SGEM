df28239aca9b0ca8b5485c157921280fcac1eadd
drumpt <ssohot1@kaist.ac.kr>
2023-02-07 04:21:24 +0900

    Fix beam-search for transducer

--- scripts/lr_scheduler_3.sh
+++ scripts/lr_scheduler_3.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python main.py \
+CUDA_VISIBLE_DEVICES=2 python main.py \
     lr=4e-5 \
     scheduler=CosineAnnealingLR \
     eta_min=2e-5 \
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     num_positives=1 \
     num_negatives=9 \
 
-CUDA_VISIBLE_DEVICES=0 python main.py \
+CUDA_VISIBLE_DEVICES=2 python main.py \
     lr=5e-5 \
     scheduler=CosineAnnealingLR \
     eta_min=2e-5 \
